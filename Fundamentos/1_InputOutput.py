f7cf2d069e5b39ae4b28f2d338b31b78ee5824fc
Janeth Castillo <janethcristy@gmail.com>
2022-02-01 20:03:50 -0500

    Fundamentos_clase2

---
+++ Fundamentos/1_InputOutput.py
@@ -0,0 +1,14 @@
+# Función de entrada y función de salida
+
+print('Formulario')
+# print('Nombre: ')
+
+# Entrada de datos
+nombre = input('Nombre: ')
+apellido = input('Apellido: ')
+edad = input('Edad: ')
+
+print('El nombre ingresado es:' ,nombre, end='\n')
+print('El apellido ingresado es:' ,apellido, end='\n')
+print('La edad ingresada es:' ,edad, end='\n')
+
---
+++ Fundamentos/2_Variables.py
@@ -0,0 +1,12 @@
+# Variables
+edad = 29
+nombre = 'Jane'
+ira = 37.2
+mayorEdad = True
+
+
+print('El tipo de dato de la variable edad es', type(edad))
+print('El tipo de dato de la variable nombre es', type(nombre))
+print('El tipo de dato de la variable ira es', type(ira))
+print('El tipo de dato de la variable mayorEdad es', type(mayorEdad))
+
