46750ce92db10eb7bed809678c8417dd4b874573
Janeth Castillo <janethcristy@gmail.com>
2022-02-01 19:00:27 -0500

    inicio

---
+++ Fundamentos/0.Prueba.py
@@ -0,0 +1,9 @@
+print("Hola Mundo 3")
+
+# Nuestro primer script
+
+# Entrada y salida de datos
+print('Formulario')
+print('Ingresa tu nombre: ')
+nombre = input()
+print('El nombre ingresado es:', nombre)
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python_curso_2022
 Curso de Python 2022
+464654
